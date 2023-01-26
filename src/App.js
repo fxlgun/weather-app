091ba51bce9f1389503d26394ace587e92239ed5
Falgun <77203052+fxlgun@users.noreply.github.com>
2023-01-26 18:33:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup, Pane } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { indianCities } from "./cityData";
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+
+    const getReq = async () => {
+      await axios.get("https://weather-42t16aeub-fxlgun.vercel.app/weather").then((response) => {
+      setData(response.data);
+    })
+      
+    }
+     getReq();
+     console.log(data);
+  }, [])
+  
+
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div style={{textAlign:"center", justifyContent:"center"}}><h1>My Weather App</h1>
+      
+      <MapContainer
+        center={{ lat: 23.0225, lng: 72.571428 }}
+        zoom={4}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        
+        {data?.map((e) => (
+          <Marker position={{ lat: e['coord']['lat'], lng: e['coord']["lon"] }}>
+            <Popup>{e["name"]}
+            <br></br>
+            Temperature: {e["main"]["temp"]}
+            <br></br>
+            Weather: {e["weather"][0]["main"]}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+      
     </div>
   );
 }
